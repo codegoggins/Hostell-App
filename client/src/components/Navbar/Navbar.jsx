2ab9088eb50382b0093646ca3af53d60e0cab1fd
Nilay Singh <snilay011@gmail.com>
2022-12-02 23:22:40 +0530

    Navbar made responsive

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React , {useState} from 'react'
 
 // CSS For Navbar Component
 import './navbar.css'
 
 const Navbar = () => {
+
+  const [toggle,setToggle] = useState(false);  
+
+
   return (
     <div className='navbar'>
         <div className="nav-container">
+
+            {/* TOGGLE NAV STARTS */}
+            
+            <ul className={`nav-toggle-list ${toggle && 'open'}`}>
+                    <li className='toggle-list-item'>
+                        <h1 className='h1'>hostell</h1>
+                    </li>
+                    <li className='toggle-list-item'>
+                        Home
+                    </li> 
+                    <li className='toggle-list-item'>
+                        Sell
+                    </li> 
+                    <li className='toggle-list-item'>
+                        Items
+                    </li> 
+                    <li className='toggle-list-item'>
+                        Login
+                    </li> 
+                    <li className='toggle-list-item'>
+                        Register
+                    </li> 
+            </ul>
+
+            {/* TOGGLE NAV ENDS */}
+
             <div className='left'>
             <h1>hostell</h1>
             </div>
@@ -24,12 +54,22 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='right'>
-                <button className='btn'>
+                <button className='nav-btn'>
                    Register
                 </button>
-                <button className='btn'>
+                <button className='nav-btn'>
                    Login
                 </button>
+            </div>
+            {/* TOGGLE BUTTONS */}
+            <div className='nav-toggle' 
+            onClick={()=>setToggle(!toggle)}
+            >
+                    {toggle ? 
+                    (<i className="fa-sharp fa-solid fa-xmark close"></i>):(
+                        <i className="fa-sharp fa-solid fa-bars"></i>
+                    )
+                    }
             </div>
         </div>
     </div>
